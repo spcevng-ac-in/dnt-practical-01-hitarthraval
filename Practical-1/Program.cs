19b5fec9f1a33d0b39cd31c35fc001094cce724d
hitarthraval <hitarthraval.hr@gmail.com>
2021-12-29 15:26:38 +0530

    Submit first practical

---
+++ Practical-1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Practical_2;
+namespace Practical_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            while(number>0)
+            {
+                int rem = number % 10;
+                sum += rem;
+                number /= 10;
+
+            }
+            Console.WriteLine("Sum=" + sum);
+            
+            
+            Console.Read();
+
+
+        }
+    }
+
+    class Test
+    {
+    }
+}
